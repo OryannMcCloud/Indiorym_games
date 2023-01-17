80595f0031ac61ca87491fa0756bc693b7e5bc70
Oryann <99395726+OryannMcCloud@users.noreply.github.com>
2023-01-17 20:33:01 +0100

    first

--- index.php
+++ index.php
@@ -3,3 +3,17 @@ echo "<h1>Welcome to Indiorym's world<h1>";
 
 
 ?>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Commerce de la rue</title>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="icon" type="image/x-icon" href="img/logo.png">
+    </head>
+  
+    <body>
+    
+    </body>
+</html> 
+
